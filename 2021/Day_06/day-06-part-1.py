4ea958c38049d1b4c3d52520afc243f4e1a1e3ad
Nick Cowans <nick.cowans@gmail.com>
2021-12-07 17:28:00 +0000

    day 7 commit

--- 2021/Day_06/day-06-part-1.py
+++ 2021/Day_06/day-06-part-1.py
@@ -1,5 +1,4 @@
 import numpy as np
-from datetime import datetime
 
 fileName = "input.txt"
 #fileName = "example.txt"
---
+++ 2021/Day_07/day-07-part-2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+fileName = "input.txt"
+#fileName = "example.txt"
+
+with open(fileName, "r") as f:
+    data = f.readlines()
+
+crabPositions = list(map(int,data[0].split(",")))
+
+minCrabPosition = min(crabPositions)
+maxCrabPosition = max(crabPositions)
+
+fuleUsedMatrix = []
+
+for i in range(minCrabPosition, maxCrabPosition+1):
+    fuelUsed = 0
+    for crab in crabPositions:
+        n = abs(crab-i)
+        fuelUsed=int(fuelUsed+((n*n+n)/2))
+    fuleUsedMatrix.append(fuelUsed)
+
+print(min(fuleUsedMatrix))
