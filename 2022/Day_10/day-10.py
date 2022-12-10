73fd3937fe21d33bcd69f6e80b884f0ab57a60f3
Nick Cowans <nick.cowans@gmail.com>
2022-12-10 08:53:57 +0000

    Day 10 - tidied up

--- 2022/Day_10/day-10.py
+++ 2022/Day_10/day-10.py
@@ -3,20 +3,15 @@ with open("input.txt", "r") as f:
     data = f.read().strip()
 
 instructions = data.split("\n")
-
-cycle = 0
 x = 1
 xHistory = [x]
 
 for instruction in instructions:
     if instruction == "noop":
-        cycle += 1
         xHistory.append(x)
     else:
         value = int(instruction.split()[1])
-        cycle += 1
         xHistory.append(x)
-        cycle += 1
         x += value
         xHistory.append(x)
 
@@ -26,16 +21,12 @@ def calScore(ns,array):
         score+=n*array[n-1]
     return score
 
-print(calScore(list(range(20,221,40)),xHistory),"\n") # Part 1
+print(calScore(list(range(20,221,40)),xHistory)) # Part 1
 
-message=""
+message="\n"
 for cycle in range(len(xHistory)-1):
     row = cycle // 40
-    if(cycle-row*40 in [xHistory[cycle]-1,xHistory[cycle],xHistory[cycle]+1]):
-        message+="#"
-    else:
-        message+=" " # nicer than .
-    if(row != ((cycle+1) // 40)):
-        message+="\n"
+    message+="##" if(cycle-row*40 in [xHistory[cycle]-1,xHistory[cycle],xHistory[cycle]+1]) else "  "
+    message+="\n" if(row != ((cycle+1) // 40)) else ""
 
-print(message) # part2
+print(message) # Part 2
