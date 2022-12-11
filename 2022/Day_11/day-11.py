5c50f8ee2279adf2c282d507138d93b02d38db17
Nick Cowans <nick.cowans@gmail.com>
2022-12-11 10:27:59 +0000

    Day 11

---
+++ 2022/Day_11/day-11.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+with open("input.txt", "r") as f:
+    data = f.read().strip()
+
+monkeyData = data.split("\n\n")
+
+items1=[[] for _ in range(len(monkeyData))]
+items2=[[] for _ in range(len(monkeyData))]
+operations=[[] for _ in range(len(monkeyData))]
+tests=[[] for _ in range(len(monkeyData))]
+trues=[[] for _ in range(len(monkeyData))]
+falses=[[] for _ in range(len(monkeyData))]
+
+for monkey in range(len(monkeyData)):
+    items1[monkey]=list(map(int,monkeyData[monkey].split("\n")[1].split(": ")[1].split(", ")))
+    items2[monkey]=list(map(int,monkeyData[monkey].split("\n")[1].split(": ")[1].split(", ")))
+    operations[monkey]=monkeyData[monkey].split("\n")[2].split("= old ")[1].split()
+    tests[monkey]=monkeyData[monkey].split("\n")[3].split("divisible by ")[1]
+    trues[monkey]=monkeyData[monkey].split("\n")[4].split("throw to monkey ")[1]
+    falses[monkey]=monkeyData[monkey].split("\n")[5].split("throw to monkey ")[1]
+
+insepctsItems1=[0 for _ in range(len(monkeyData))]
+insepctsItems2=[0 for _ in range(len(monkeyData))]
+
+commonMultiplier = 1
+for test in tests:
+    commonMultiplier *= int(test)
+
+
+def runPart(part=1, noRounds=20, items=items1, insepctsItems=insepctsItems1):
+    for _ in range(noRounds):
+        for monkey in range(len(monkeyData)):
+            for item in items[monkey]:
+                insepctsItems[monkey]+=1
+                mult = int(item) if operations[monkey][1]=="old" else int(operations[monkey][1])
+                new = int(item)+mult if operations[monkey][0]=="+" else int(item)*mult
+                new = new // 3 if part==1 else new % commonMultiplier # remainder is what matters and keeps numbers down
+                newMonkey = int(trues[monkey]) if new % int(tests[monkey]) == 0 else int(falses[monkey])
+                items[newMonkey].append(new)
+            items[monkey]=[]
+    return sorted(insepctsItems,reverse=True)[0]*sorted(insepctsItems,reverse=True)[1]
+
+print(runPart(part=1, noRounds=20, items=items1, insepctsItems=insepctsItems1))
+print(runPart(part=2, noRounds=10000, items=items2, insepctsItems=insepctsItems2))
