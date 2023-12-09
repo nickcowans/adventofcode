f880442a5a55ea3d56348d9c470d0dfaeb7119b7
Nick Cowans <nick.cowans@gmail.com>
2023-12-09 09:02:29 +0000

    2023, Day 5, Part 1

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -3,13 +3,6 @@ input = open("in.txt").read().strip().split('\n\n')# Read in the input
 seeds=[int(seed) for seed in input[0].split("seeds: ")[1].split()]
 instructions=input[1:]
 
-def inOut(D, S, R, inVal):
-    if((int(inVal) >= int(S)) & (int(inVal) < (int(S)+int(R)))):
-        outVal=int(D)+int(inVal)-int(S)
-        return outVal
-    else:
-        return False
-
 finals=[]
 for inVal in seeds:
     for instruction in instructions:
@@ -19,8 +12,8 @@ for inVal in seeds:
         i = 0
         maxI = len(mapCodesList)
         while (newVal == False and i < maxI):
-            D, S, R = mapCodesList[i].split() # Destination Start, Source Start, Range
-            newVal = inOut(D, S, R, inVal)
+            D, S, R = map(int, mapCodesList[i].split()) # Destination Start, Source Start, Range
+            newVal = D + inVal - S if S <= inVal <= S + R else False
             i+=1
         if newVal == False:
             newVal = inVal
